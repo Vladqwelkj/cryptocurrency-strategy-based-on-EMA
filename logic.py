b0e204480edb293aa8bd37efe97b240b53426f65
- Vladqwelkj <vladkanal@gmail.com>
2019-09-16 20:11:49 +0700

    finished

--- logic.py
+++ logic.py
@@ -17,24 +17,25 @@ def addPoint(pose=None, index=None, price=None, chart_on=True):
 			ax.scatter([index], [price], s = 50, color = 'yellow', alpha = 0.8)
 
 
-
-
-
 def backtest(H,L,C,
 			src_enter_long, src_enter_short,
 			src_sma,
 			sma_n, atr_n, confirm_n, atr_stop_x,
 			parts_exit,
 			fee_limit=-0.00025, fee_market=0.00075, slip=3.0,
-			chart_on=True, one_deal=1000):
+			chart_on=True, procent_one_deal=1):
 
 	sma = ta_indicators.SMA(src_sma, sma_n)
 	atr = ta_indicators.ATR(atr_n, H, L)
 
 	balance = 1000
-	one_deal = 1000
 	losses, profits = 0, 0
 
+	fee_limit *= procent_one_deal
+	fee_market *= procent_one_deal
+
+	max_papers_profits = []
+	tmp_max_paper_profits = []
 	balance_changes = []
 	sum_parts = sum(parts_exit.values())
 	position = {'amount': 0, 'first': True, 'is_long': None}#enter, takes[], stop, is_finished
@@ -49,9 +50,11 @@ def backtest(H,L,C,
 			result = False
 ########
 			if position['is_long']:
+				tmp_max_paper_profits.append(H[i]/position['enter']-1)
+
 				if L[i] < position['stop']: #STOP trigger
 					addPoint(pose='Stop', index=i, price=position['stop'], chart_on=chart_on)
-					result = (((position['stop'] - slip) / (position['enter'] + slip))
+					result = (((position['stop']) / (position['enter']))
 						- (fee_market+fee_limit)/100) * position['amount'] - position['amount']
 					position['amount'] = 0
 
@@ -60,7 +63,7 @@ def backtest(H,L,C,
 				for take in position['takes']:
 					if H[i] > take['price'] and not take['is_filled']:
 						addPoint(pose='Take', index=i, price=take['price'], chart_on=chart_on)
-						result = ((take['price'] / (position['enter'] + slip))
+						result = ((take['price'] / (position['enter']))
 							- fee_limit/100*2) * take['amount'] - take['amount']
 						position['amount'] = position['amount'] - take['amount']
 						take['is_filled'] = True
@@ -69,13 +72,16 @@ def backtest(H,L,C,
 						else:
 							losses += result
 						balance += result
-						balance_changes.append(balance)
+						#balance_changes.append(balance)
 						result = False
 ########
-			if not position['is_long']:
+			if not position['is_long']: #if short
+
+				tmp_max_paper_profits.append(position['enter']/L[i]-1)
+
 				if H[i] > position['stop']: #STOP trigger
 					addPoint(pose='Stop', index=i, price=position['stop'], chart_on=chart_on)
-					result = (((position['enter'] - slip) / (position['stop'] + slip))
+					result = (((position['enter']) / (position['stop']))
 						- (fee_market+fee_limit)/100) * position['amount'] - position['amount']
 					position['amount'] = 0
 
@@ -84,7 +90,7 @@ def backtest(H,L,C,
 				for take in position['takes']:
 					if L[i] < take['price']  and not take['is_filled']:
 						addPoint(pose='Take', index=i, price=take['price'], chart_on=chart_on)
-						result = ((position['enter'] - slip) / (take['price'])
+						result = (position['enter'] / take['price']
 							- fee_limit/100*2) * take['amount'] - take['amount']
 						position['amount'] = position['amount'] - take['amount']
 						take['is_filled'] = True
@@ -93,7 +99,7 @@ def backtest(H,L,C,
 						else:
 							losses += result
 						balance += result
-						balance_changes.append(balance)
+						#balance_changes.append(balance)
 						result = False
 			if result:
 				if result > 0:
@@ -116,21 +122,23 @@ def backtest(H,L,C,
 		if is_long and (position['first'] or not position['is_long']):
 			try:
 				position['enter'] #если первый ордер - на этом этапе вылетит исключение
-				result = ((position['enter'] - slip) / (close + slip) - fee_limit/100*2) * position['amount'] - position['amount']
+				result = ((position['enter']) / (close) - fee_limit/100*2) * position['amount'] - position['amount']
 				if result > 0:
 					profits += result
 				else:
 					losses += result
 				balance += result
 				balance_changes.append(balance)
+				max_papers_profits.append(max(tmp_max_paper_profits))
+				tmp_max_paper_profits = []
 			except:
 				pass
 			addPoint(pose='Long', index=i, price=close, chart_on=chart_on)
-			position = {'is_long': True, 'enter': close, 'takes':[], 'stop':close-atr[i]*atr_stop_x, 'amount': one_deal, 'first': False}
+			position = {'is_long': True, 'enter': close-slip, 'takes':[], 'stop':close-atr[i]*atr_stop_x, 'amount': balance*procent_one_deal, 'first': False}
 
 			for proc in parts_exit.keys():
 				position['takes'].append({'price': close*(1+proc/100),
-										 'amount': parts_exit[proc]/sum_parts *one_deal,
+										 'amount': parts_exit[proc]/sum_parts *balance*procent_one_deal,
 										 'is_filled': False})
 
 
@@ -139,29 +147,36 @@ def backtest(H,L,C,
 		if is_short and (position['first'] or position['is_long']):
 			try:
 				position['enter'] #если первый ордер - на этом этапе вылетит исключение
-				result = ((close - slip) / (position['enter'] + slip) - fee_limit/100*2) * position['amount'] - position['amount']
+				result = ((close + slip) / (position['enter']) - fee_limit/100*2) * position['amount'] - position['amount']
 				if result > 0:
 					profits += result
 				else:
 					losses += result
 				balance += result
 				balance_changes.append(balance)
-
+				max_papers_profits.append(max(tmp_max_paper_profits))
+				tmp_max_paper_profits = []
 			except:
 				pass
 			addPoint(pose='Short', index=i, price=close, chart_on=chart_on)
-			position = {'is_long': False, 'enter': close, 'takes':[], 'stop':close+atr[i]*atr_stop_x, 'amount': one_deal, 'first': False}
+			position = {'is_long': False, 'enter': close+slip, 'takes':[], 'stop':close+atr[i]*atr_stop_x, 'amount': balance*procent_one_deal, 'first': False}
 
 			for proc in parts_exit.keys():
 				position['takes'].append({'price': close*(1-proc/100),
-										 'amount': parts_exit[proc]/sum_parts *one_deal,
+										 'amount': parts_exit[proc]/sum_parts *balance*procent_one_deal,
 										 'is_filled': False})
 
 
 	if chart_on:
 		for src in [H, L, C, sma]:
 			pylab.plot(src, alpha=0.3)
+		f = open('balance.txt', 'w')
+		for b in balance_changes:
+			f.write(str(b)+'\n')
 		pylab.show()
 		pylab.plot(balance_changes)
 		pylab.show()
+		pylab.plot(sorted(max_papers_profits))
+		pylab.show()
+
 	return {'balance': balance, 'factor': profits/-losses}
--- set_and_start.py
+++ set_and_start.py
@@ -2,30 +2,29 @@ from get_data import get_data
 from logic import backtest
 
 parts_exit = {# proc_exit, part_of_position
-	0.5: 100,
-1.0: 2,
-1.5: 1,
-2.0: 1,
+	0.5: 5,
+0.8: 4,
+1.4: 3,
+1.9: 2,
 2.5: 1,
-3.0: 1,
-3.5: 1,
-#90:  500
-	}
-'''
+3: 5,
+5: 5,
+10:10
 
-'''
-OHLC = get_data(3, 30000, 'PriceData/15min.txt')
+	}
+
+OHLC = get_data(3, 300000, 'txt.txt')
 result = backtest(H=OHLC['high'], L=OHLC['low'], C=OHLC['close'],
 			src_enter_long=OHLC['close'], src_enter_short=OHLC['close'],
 			src_sma=OHLC['close'],
 
 			sma_n=31,
 			atr_n=35,
-			confirm_n=3,
+			confirm_n=2,
 			atr_stop_x=5,
 
 			parts_exit=parts_exit,
-			fee_limit=-0.025, fee_market=0.075, slip=300.0,
-			chart_on=True, one_deal =15000)
+			fee_limit=-0.015, fee_market=0.08, slip=-0*10000,
+			chart_on=True, procent_one_deal =1)
 
 print(result)
